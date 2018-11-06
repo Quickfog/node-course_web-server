1f9f5dab5d6bd980b92b24c7f8fab3f868f34a81
unknown <yaronfog@gmail.com>
2018-11-06 19:51:29 +0200

    added Projects page

--- server.js
+++ server.js
@@ -35,9 +35,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  // res.send('about page');
   res.render('about.hbs', {
     pageTitle: 'About page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
